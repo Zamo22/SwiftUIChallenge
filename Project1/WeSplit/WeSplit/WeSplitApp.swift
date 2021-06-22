d8fd1329f34efcc4865c498dc2debaab4cd20517
Zaheer <ZMoola@jhb.dvt.co.za>
2021-06-22 22:49:14 +0200

    Day 16 & 17 of 100 day challenge

---
+++ Project1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Zaheer Moola on 2021/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
