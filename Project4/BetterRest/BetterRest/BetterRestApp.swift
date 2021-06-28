b3d5bb7c87c71e481df934845bac75d625abd871
Zaheer <ZMoola@jhb.dvt.co.za>
2021-06-28 21:57:07 +0200

    Started the Better Rest project 4

---
+++ Project4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zaheer Moola on 2021/06/28.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
