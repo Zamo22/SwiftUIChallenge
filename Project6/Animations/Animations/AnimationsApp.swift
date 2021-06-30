7e958fe9903ef4cb85b3160ed6e3f1f20c3beb8d
Zaheer <ZMoola@jhb.dvt.co.za>
2021-06-30 21:39:17 +0200

    Completed project 6 Most of the tutorials are commented out

---
+++ Project6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Zaheer Moola on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
