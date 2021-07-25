b93ed8f64ad8acbf17a04d8149672012cea2ac2c
Zaheer <ZMoola@jhb.dvt.co.za>
2021-07-25 14:54:41 +0200

    Completed project 13 plus challenge

--- Project13/InstaFilter/InstaFilter/ContentView.swift
+++ Project13/InstaFilter/InstaFilter/ContentView.swift
@@ -11,26 +11,159 @@ import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
-        @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
 
-        var body: some View {
+    @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var showingImagePicker = false
+
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+
+    @State private var showingFilterSheet = false
+    @State private var showErrorAlert = false
+
+    var body: some View {
+        let intensity = Binding<Double>(
+            get: { filterIntensity },
+            set: {
+                filterIntensity = $0
+                applyProcessing()
+            }
+        )
+
+        let radius = Binding<Double>(
+            get: { filterRadius },
+            set: {
+                filterRadius = $0
+                applyProcessing()
+            }
+        )
+
+        NavigationView {
             VStack {
-                image?
-                    .resizable()
-                    .scaledToFit()
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
 
-                Button("Select Image") {
-                   self.showingImagePicker = true
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select and image")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intensity)
+                }.padding(.vertical)
+
+                HStack {
+                    Text("Radius")
+                    Slider(value: radius)
+                }.padding(.vertical)
+
+                HStack {
+                    Button(currentFilter.name) {
+                        showingFilterSheet = true
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                        guard let processedImage = processedImage else {
+                            showErrorAlert = true
+                            return
+                        }
+                        let imageSaver = ImageSaver()
+
+                        imageSaver.errorHandler = {
+                            debugPrint("Oops: \($0.localizedDescription)")
+                        }
+
+                        imageSaver.successHandler = {
+                            debugPrint("Saved!")
+                        }
+
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
+                    }
                 }
             }
-            .sheet(isPresented: $showingImagePicker) {
-                ImagePicker()
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("InstaFilter")
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: $inputImage)
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                    .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                    .cancel()
+                ])
+            }
+            .alert(isPresented: $showErrorAlert) {
+                Alert(title: Text("Error!"),
+                      message: Text("You have not selected an image"),
+                      dismissButton: .default(Text("Ok")))
             }
         }
+    }
+
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            processedImage = uiImage
+            image = Image(uiImage: uiImage)
+        }
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?,
+                         contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
     }
 }
